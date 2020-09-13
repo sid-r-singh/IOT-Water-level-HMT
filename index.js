c9665f81535257eba9c73abe1601e46b35a0605e
sid-r-singh <ofw3210@gmail.com>
2020-09-13 10:02:25 +0530

    3 files modified, @firebase/app added

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import firebase from 'firebase';
 firebase.initializeApp({
   databaseURL: 'https://mt-iot-brn.firebaseio.com/',
 });
-
+let ref = firebase.database().ref('Tanks');
+const app2 = document.getElementById('app2');
+ref.on('value',(snapshot) => {
+app2.innerHTML = snapshot.child('tank1').val();
+});
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Tank water level</h1>`;
